4d373444c9705566a6c16a9d487618f1c2b486d0
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-09-09 00:07:57 +0200

    style: use arrow functions

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -1,22 +1,14 @@
-import { FontAwesome } from "@expo/vector-icons";
-import * as Font from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect, useState } from "react";
 
-export default function useCachedResources() {
+const useCachedResources = () => {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
 
   // Load any resources or data that we need prior to rendering the app
   useEffect(() => {
-    async function loadResourcesAndDataAsync() {
+    const loadResourcesAndDataAsync = async () => {
       try {
         SplashScreen.preventAutoHideAsync();
-
-        // Load fonts
-        await Font.loadAsync({
-          ...FontAwesome.font,
-          "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf"),
-        });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
         console.warn(e);
@@ -24,10 +16,12 @@ export default function useCachedResources() {
         setLoadingComplete(true);
         SplashScreen.hideAsync();
       }
-    }
+    };
 
     loadResourcesAndDataAsync();
   }, []);
 
   return isLoadingComplete;
-}
+};
+
+export default useCachedResources;
