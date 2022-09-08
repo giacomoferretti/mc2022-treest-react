4b5cc1bdc7ff4a553838ebd7dafb905927899946
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-09-08 23:55:19 +0200

    feat: add useAsyncStorage hook

---
+++ src/hooks/useAsyncStorage.ts
@@ -0,0 +1,39 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+
+const useAsyncStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T>(initialValue);
+
+  useEffect(() => {
+    try {
+      AsyncStorage.getItem(key)
+        .then((value) => {
+          console.log("[AsyncStorage] Loading", value);
+
+          if (value === null) return initialValue;
+          return JSON.parse(value);
+        })
+        .then(setStoredValue);
+    } catch (e) {
+      console.error(e);
+    }
+  }, [key, initialValue]);
+
+  const setValue = (value: T | ((val: T) => T)) => {
+    const valueToStore = value instanceof Function ? value(storedValue) : value;
+
+    console.log("[AsyncStorage] Saving", valueToStore);
+
+    setStoredValue(valueToStore);
+
+    try {
+      AsyncStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return [storedValue, setValue] as const;
+};
+
+export default useAsyncStorage;
