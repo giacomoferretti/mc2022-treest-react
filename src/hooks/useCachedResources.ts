8831ded1dca9c1710f618e00364552f206326e54
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2022-09-14 03:11:02 +0200

    fix: add Material fonts to startup load

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -1,3 +1,5 @@
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import * as Font from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect, useState } from "react";
 
@@ -9,6 +11,14 @@ const useCachedResources = () => {
     const loadResourcesAndDataAsync = async () => {
       try {
         SplashScreen.preventAutoHideAsync();
+
+        await Font.loadAsync({
+          ...MaterialIcons.font,
+          ...MaterialCommunityIcons.font,
+          // "Inter-Regular": require("../../assets/fonts/Inter-Regular.otf"),
+          // "Inter-Bold": require("../../assets/fonts/Inter-Bold.otf"),
+          // "Inter-Black": require("../../assets/fonts/Inter-Black.otf"),
+        });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
         console.warn(e);
